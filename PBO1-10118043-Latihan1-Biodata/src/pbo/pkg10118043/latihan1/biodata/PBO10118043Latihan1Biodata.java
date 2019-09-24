1898e1317d76a10f1dbeed0db032d170e4a739c4
DianRP43 <55683288+DianRP43@users.noreply.github.com>
2019-09-24 14:55:18 +0700

    Add files via upload
    PBO1 = IF-01

---
+++ PBO1-10118043-Latihan1-Biodata/src/pbo/pkg10118043/latihan1/biodata/PBO10118043Latihan1Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118043.latihan1.biodata;
+
+/**
+ *
+ * @author user
+ */
+public class PBO10118043Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA DIAN" + "\n" + "--------------------");
+        System.out.println("Nama\t\t: Dian Rosa Pratama");
+        System.out.println("Alamat\t\t: Kopo Jaya");
+        System.out.println("Tempat Lahir\t: Ciamis");
+        System.out.println("Tanggal Lahir\t: 06 11 2000");
+        System.out.println("----------------------");
+        System.out.println("Hobi\t\t: Catur");
+        System.out.println("Makes\t\t: App");
+        System.out.println("Mikes\t\t: Juz");
+       
+    }
+    
+}
